1ad8bd027d5a4a43ea2071f929fef365a9905891
copernicus <nclee2@asu.edu>
2016-04-21 00:13:44 -0700

    cleaned sandbox

--- Aurora4xAutomation/Tests/Sandbox.cs
+++ Aurora4xAutomation/Tests/Sandbox.cs
@@ -17,21 +17,8 @@ namespace Aurora4xAutomation.Tests
         [Test]
         public void MyTest()
         {
-            var accessConnStr = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\prog\Aurora\Aurora_latest\Aurora\Stevefire.mdb;Jet OLEDB:Database Password=raistlin31";
-            var getEventsQuery = @"SELECT * FROM GameLog";
 
-            var myDataSet = new DataSet();
-            var accessConn = new OleDbConnection(accessConnStr);
 
-            var myAccessCommand = new OleDbCommand(getEventsQuery, accessConn);
-            var myDataAdapter = new OleDbDataAdapter(myAccessCommand);
-
-            accessConn.Open();
-            myDataAdapter.Fill(myDataSet);
-            var r = myDataSet.Tables[0].Rows;
-            var taurora = r[r.Count-1]["Time"];
-            var treal = DateTime.FromOADate((double) taurora);
-            accessConn.Close();
         }
     }
 }
