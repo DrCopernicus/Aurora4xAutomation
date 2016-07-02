8abc187d3c60a52368f158a0d8a2dbe084cf39f4
copernicus <nclee2@asu.edu>
2016-07-01 19:49:31 -0700

    Removes Game/ and REST/ classes related to Game objects.

--- Aurora4xAutomation/Game/AuroraGame.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Aurora4xAutomation.Game
-{
-    public static class AuroraGame
-    {
-        private static readonly Empire _empire = new Empire();
-        public static Empire Empire { get { return _empire; } }
-    }
-}
